e4e386c5c8d9f4d8cd1350eb0fc59aa229604267
codeabuu <abdulngui718@gmail.com>
2024-01-18 22:51:20 +0000

    minimum ops required to reach a letter

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,26 +3,10 @@
 
 
 def minOperations(n):
-    """ Calculates the fewest number of operations needed
-        to result in exactly n H characters in the file
-
-        Args:
-            n: repetitions of H
-
-        Returns:
-            number of operations (Copy & Paste) to reach n Hs
-    """
-    operations = 0
-    summation = 1
-    carrier = 0
-
-    while summation < n:
-        if n % summation == 0:  # Copy when summation is a multiple of n
-            carrier = summation
-            summation *= 2
-            operations += 1
-        else:
-            summation += carrier
-        operations += 1  # Always paste
-
-    return operations
+    '''return'''
+    if n <= 1:
+        return 0
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return i + minOperations(n // i)
+    return n
