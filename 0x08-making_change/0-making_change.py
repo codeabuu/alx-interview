0d6f2ad00a3c7b84c3fd617e3cc81a2e5f0cdd75
codeabuu <abdulngui718@gmail.com>
2024-02-29 21:17:35 +0000

    change coins with test case

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-'''solving change coins challenge'''
+"""Change making module.
+"""
 
 
 def makeChange(coins, total):
-    '''determine the fewest number of coins needed to meet a given amount total'''
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    if dp[total] == float('inf'):
-        return -1
-    else:
-        return dp[total]
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
