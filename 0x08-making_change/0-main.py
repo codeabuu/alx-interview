775ba76b736a010da9bc13190bd86f2d93a5a6dd
codeabuu <abdulngui718@gmail.com>
2024-02-29 21:08:12 +0000

    change coin challenge

---
+++ 0x08-making_change/0-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+makeChange = __import__('0-making_change').makeChange
+
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''solving change coins challenge'''
+
+
+def makeChange(coins, total):
+    '''determine the fewest number of coins needed to meet a given amount total'''
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
